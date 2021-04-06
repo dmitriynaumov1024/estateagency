ab3165e428b0e2c040929de590fc8c3e95a67f06
dmitriynaumov1024 <naumov1024@gmail.com>
2021-04-06 18:57:47 +0300

    Changed route map

--- EstateAgency/Startup.cs
+++ EstateAgency/Startup.cs
@@ -35,7 +35,7 @@ namespace EstateAgency
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -50,7 +50,8 @@ namespace EstateAgency
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Homepage" });
             });
         }
     }
