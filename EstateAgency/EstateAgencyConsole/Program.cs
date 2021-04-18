573c6e885ba9f21d348023bb1a3527474f598b24
dmitriynaumov1024 <naumov1024@gmail.com>
2021-04-18 11:52:45 +0300

    'admin panel'
    I added admin panel project.

---
+++ EstateAgency/EstateAgencyConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EstateAgencyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
