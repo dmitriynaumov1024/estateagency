939e2b3a7b265196dbb3535791afbea56548cb29
dmitriynaumov1024 <naumov1024@gmail.com>
2021-04-18 15:35:36 +0300

    Db
    Now I do db in thick client mode. Also added some gitingore

---
+++ EstateAgency/Entities/Classes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Entities
+{
+    public class Person
+    {
+        public string Surname;
+        public string Name;
+        public string Email;
+        public string Phone;
+        public int LocationID;
+        public string Address;
+        public DateTime RegDate;
+    }
+
+    public class Location
+    {
+        public string Region;
+        public string Town;
+        public string District;
+    }
+
+    public class Credential
+    {
+        public int PersonID;
+        public string Password;
+        public char Privilegies;
+        public char Status;
+    }
+}
--- EstateAgency/EstateAgencyConsole/Program.cs
+++ EstateAgency/EstateAgencyConsole/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using Apache.Ignite;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache;
+using Entities;
 
 namespace EstateAgencyConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        public static IIgnite StartIgnitePersistent()
         {
-            Console.WriteLine("Hello World!");
+            IgniteConfiguration cfg = new IgniteConfiguration 
+            { 
+                RedirectJavaConsoleOutput = false,
+                DataStorageConfiguration = 
+                new Apache.Ignite.Core.Configuration.DataStorageConfiguration
+                {
+                    StoragePath = "/storage",
+                    WalMode = Apache.Ignite.Core.Configuration.WalMode.Background,
+                    WalSegments = 2,
+                    WalHistorySize = 2,
+                    DefaultDataRegionConfiguration = 
+                    new Apache.Ignite.Core.Configuration.DataRegionConfiguration
+                    {
+                        PersistenceEnabled = true,
+                        Name = "defaultregion"
+                    }
+                }
+            };
+            IIgnite ignite = Ignition.Start(cfg);
+            ignite.GetCluster().SetActive(true);
+            return ignite;
+        }
+
+        static void Main ()
+        {
+            IIgnite ignite = StartIgnitePersistent();
+            ICache<int, Person> Persons = ignite.GetOrCreateCache<int, Person>("persons");
+            Person p = null;
+
+            
+            Person p0 = new Person
+            {
+                Surname = "Іванов",
+                Name = "Іван",
+                Email = "ivanov.ivan@ukr.net",
+                Phone = "+380964449810",
+                LocationID = 1,
+                Address = "вул. Житомирська, буд. 6",
+                RegDate = DateTime.Today
+            };
+            ignite.GetCluster().DisableWal("persons");
+            for (int i=-10001; i<20000; i++)
+            Persons.Put (i, p0);
+            ignite.GetCluster().EnableWal("persons");
+
+            if (Persons.TryGet (0, out p) && p!=null)
+            {
+                Console.WriteLine ($"Surname: {p.Surname} \nName: {p.Name} \nSignup date: {p.RegDate}");
+            } 
+            else
+            {
+                Console.WriteLine ("Key not found.");
+            }
+            Console.Read();
+            ignite.Dispose();
         }
     }
 }
