b3e3726460d460a31817e1b76ccc1b0b340adc18
dmitriynaumov1024 <naumov1024@gmail.com>
2021-04-24 16:51:18 +0300

    Db implementation

--- EstateAgency/Entities/Classes.cs
+++ EstateAgency/Entities/Classes.cs
@@ -1,16 +1,62 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
+/// <summary>
+/// Represents a set of Entities of Estate agency.
+/// </summary>
 namespace Entities
 {
+    /// <summary>
+    /// Credential. Contains back link to Person, 
+    /// information about Password, privilegies and status.
+    /// </summary>
+    public class Credential
+    {
+        /// <summary>
+        /// Back link to Person. Mandatory field. Credential can not exist without a Person.
+        /// </summary>
+        public int PersonID;
+
+        /// <summary>
+        /// Password.
+        /// </summary>
+        public string Password;
+
+        /// <summary>
+        /// Privilegies. Currently possible values are: <br/>
+        /// 'c' - client <br/>
+        /// 'a' - agent  <br/>
+        /// 'm' - moderator <br/>
+        /// 'r' - root. 
+        /// </summary>
+        public byte Privilegies;
+
+        /// <summary>
+        /// Status of account. Currently possible values are: <br/>
+        /// 'n' - normal <br/>
+        /// 'd' - deactivated <br/>
+        /// 'b' - banned.
+        /// </summary>
+        public byte Status;
+    }
+
     public class Person
     {
         public string Surname;
         public string Name;
-        public string Email;
         public string Phone;
+        public string Email;
         public int LocationID;
         public string Address;
         public DateTime RegDate;
+    }
+
+    public class Agent
+    {
+        public int TotalDeals;
+        public int MonthDeals;
+        public int MonthPayment;
     }
 
     public class Location
@@ -20,11 +66,81 @@ namespace Entities
         public string District;
     }
 
-    public class Credential
+    public class EstateObject
+    {
+        public int SellerID;
+        public DateTime PostDate;
+        public bool isOpen;
+        public bool isVisible;
+        public int LocationID;
+        public string Address;
+        public byte Variant;
+        public int Price;
+        public byte State;
+        public string Description;
+        public List<string> Tags;
+        public List<string> PhotoUrls;
+    }
+
+    public class House
+    {
+        public float LandArea;
+        public float HomeArea;
+        public int FloorCount;
+        public int RoomCount;
+    }
+
+    public class Flat
+    {
+        public int Floor;
+        public float HomeArea;
+        public int RoomCount;
+    }
+
+    public class Landplot
+    {
+        public float LandArea;
+    }
+
+    public class ClientWish
+    {
+        public int ClientID;
+        public bool isOpen;
+        public DateTime PostDate;
+        public int LocationID;
+        public char Variant;
+        public int Price;
+        public List<string> Tags;
+        public byte NeededState;
+    }
+
+    public class Bookmark
     {
         public int PersonID;
-        public string Password;
-        public char Privilegies;
-        public char Status;
+        public int ObjectID;
+    }
+
+    public class Match
+    {
+        public int WishID;
+        public int ObjectID;
+    }
+
+    public class Order
+    {
+        public int ClientID;
+        public int ObjectID;
+        public int AgentID;
+        public DateTime OrderTime;
+        public bool isOpen;
+    }
+
+    public class Deal
+    {
+        public int BuyerID;
+        public int SellerID;
+        public int AgentID;
+        public DateTime DealTime;
+        public int Price;
     }
 }
--- EstateAgency/EstateAgencyConsole/Program.cs
+++ EstateAgency/EstateAgencyConsole/Program.cs
@@ -1,13 +1,306 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Apache.Ignite;
 using Apache.Ignite.Core;
-using Apache.Ignite.Core.Cache;
+using Apache.Ignite.Core.Cache.Query;
+using Apache.Ignite.Core.Client;
+using Apache.Ignite.Core.Client.Cache;
+using Apache.Ignite.Core.Cache.Configuration;
 using Entities;
 
 namespace EstateAgencyConsole
 {
     class Program
     {
+        /// <summary>
+        /// Method for creating a database of Estate agency.
+        /// </summary>
+        /// <param name="client">IIgniteClient instance for database connection.</param>
+        /// <returns>true if caches were successfully created, false otherwise.</returns>
+        public static bool CreateDatabase(IIgniteClient client)
+        {
+            CacheClientConfiguration CacheCfg = new CacheClientConfiguration
+            {
+                Name = "EstateAgency",
+                QueryEntities = new[]
+                {
+                    new QueryEntity
+                    {
+                        TableName = "Persons",
+                        KeyType = typeof(int),
+                        ValueType = typeof(Person),
+                        Fields = new QueryField[]
+                        {
+                            new QueryField ("Surname", typeof(string)),
+                            new QueryField ("Name", typeof(string)),
+                            new QueryField ("Phone", typeof(string)),
+                            new QueryField ("Email", typeof(string)),
+                            new QueryField ("LocationID", typeof(int)),
+                            new QueryField ("Address", typeof(string)),
+                            new QueryField ("RegDate", typeof(DateTime))
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "Locations",
+                        KeyType = typeof(int),
+                        ValueType = typeof(Location),
+                        Fields = new QueryField[]
+                        {
+                            new QueryField ("Region", typeof(string)),
+                            new QueryField ("Town", typeof(string)),
+                            new QueryField ("District", typeof(string))
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "Credentials",
+                        KeyType = typeof(string),
+                        ValueType = typeof(Credential)
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "Agents",
+                        KeyType = typeof(int),
+                        ValueType = typeof(Agent),
+                        Fields = new QueryField[]
+                        {
+                            new QueryField ("TotalDeals", typeof(int)),
+                            new QueryField ("MonthDeals", typeof(int)),
+                            new QueryField ("MonthPayment", typeof(int))
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "EstateObjects",
+                        KeyType = typeof(int),
+                        ValueType = typeof(EstateObject),
+                        Fields = new QueryField[]
+                        {
+                            new QueryField ("SellerID", typeof(int)),
+                            new QueryField ("PostDate", typeof(DateTime)),
+                            new QueryField ("isOpen", typeof(bool)),
+                            new QueryField ("isVisible", typeof(bool)),
+                            new QueryField ("LocationID", typeof(int)),
+                            new QueryField ("Address", typeof(string)),
+                            new QueryField ("Variant", typeof(byte)),
+                            new QueryField ("Price", typeof(int)),
+                            new QueryField ("State", typeof(byte)),
+                            new QueryField ("Description", typeof(string)),
+                            new QueryField ("Tags", typeof (List<string>)),
+                            new QueryField ("PhotoUrls", typeof (List<string>))
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "Houses",
+                        KeyType = typeof(int),
+                        ValueType = typeof(House),
+                        Fields = new QueryField[]
+                        {
+                            new QueryField ("LandArea", typeof(float)),
+                            new QueryField ("HomeArea", typeof(float)),
+                            new QueryField ("FloorCount", typeof(int)),
+                            new QueryField ("RoomCount", typeof(int))
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "Flats",
+                        KeyType = typeof(int),
+                        ValueType = typeof(Flat),
+                        Fields = new QueryField[]
+                        {
+                            new QueryField ("Floor", typeof(int)),
+                            new QueryField ("HomeArea", typeof(float)),
+                            new QueryField ("RoomCount", typeof(int))
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "LandPlots",
+                        KeyType = typeof(int),
+                        ValueType = typeof(Landplot),
+                        Fields = new[]
+                        {
+                            new QueryField ("LandArea", typeof(float))
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "Deals",
+                        KeyType = typeof(int),
+                        ValueType = typeof(Deal),
+                        Fields = new[]
+                        {
+                            new QueryField ("BuyerID", typeof(int)),
+                            new QueryField ("SellerID", typeof(int)),
+                            new QueryField ("AgentID", typeof(int)),
+                            new QueryField ("DealTime", typeof(DateTime)),
+                            new QueryField ("Price", typeof(int))
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "ClientWishes",
+                        KeyType = typeof(int),
+                        ValueType = typeof(ClientWish),
+                        Fields = new[]
+                        {
+                            new QueryField ("ClientID", typeof(int)),
+                            new QueryField ("isOpen", typeof(bool)),
+                            new QueryField ("PostDate", typeof(DateTime)),
+                            new QueryField ("LocationID", typeof(int)),
+                            new QueryField ("Variant", typeof(byte)),
+                            new QueryField ("Price", typeof(int)),
+                            new QueryField ("Tags", typeof(List<string>)),
+                            new QueryField ("NeededState", typeof(byte))
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "Matches",
+                        KeyType = typeof(int),
+                        ValueType = typeof(Match),
+                        Fields = new[]
+                        {
+                            new QueryField ("WishID", typeof(int)),
+                            new QueryField ("ObjectID", typeof(int))
+                        },
+                        Indexes = new[]
+                        {
+                            new QueryIndex ("WishID"),
+                            new QueryIndex ("ObjectID")
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "Bookmarks",
+                        KeyType = typeof(int),
+                        ValueType = typeof(Bookmark),
+                        Fields = new[]
+                        {
+                            new QueryField ("PersonID", typeof(int)),
+                            new QueryField ("ObjectID", typeof(int))
+                        }
+                    },
+                    new QueryEntity
+                    {
+                        TableName = "Orders",
+                        KeyType = typeof(int),
+                        ValueType = typeof(Order),
+                        Fields = new[]
+                        {
+                            new QueryField ("ClientID", typeof(int)),
+                            new QueryField ("ObjectID", typeof(int)),
+                            new QueryField ("AgentID", typeof(int)),
+                            new QueryField ("OrderTime", typeof(DateTime)),
+                            new QueryField ("isOpen", typeof(bool))
+                        }
+                    }
+                }
+            };
+
+
+            try 
+            { 
+                client.CreateCache<int, Person>(CacheCfg);
+                // Table/cache deployment goes here.
+                /*
+                client.CreateCache<string, Credential>(CacheCfg);
+                client.CreateCache<int, Person>(CacheCfg);
+                client.CreateCache<int, Agent>(CacheCfg);
+                client.CreateCache<int, Location>(CacheCfg);
+                client.CreateCache<int, EstateObject>(CacheCfg);
+                client.CreateCache<int, House>(CacheCfg);
+                client.CreateCache<int, Flat>(CacheCfg);
+                client.CreateCache<int, Landplot>(CacheCfg);
+                client.CreateCache<int, Deal>(CacheCfg);
+                client.CreateCache<int, Bookmark>(CacheCfg);
+                client.CreateCache<int, Order>(CacheCfg);
+                client.CreateCache<int, ClientWish>(CacheCfg);
+                client.CreateCache<int, Match>(CacheCfg);
+                */
+
+                // Return true if all tables/caches with given configurations are successfully created
+                return true;
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception occured during cache creation. \n Details: \n");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+            
+        }
+
+        /// <summary>
+        /// Connect to Apache ignite cluster.
+        /// </summary>
+        /// <returns>Instance of IIgniteClient</returns>
+        public static IIgniteClient GetClient()
+        {
+            var igniteCfg = new IgniteClientConfiguration
+            {
+                Endpoints = new[] {"127.0.0.1:10800"}
+            };
+            return Ignition.StartClient (igniteCfg);
+        }
+
+        /// <summary>
+        /// Main entry point for application. The main task 
+        /// of the program is to create database of Estate agency.
+        /// </summary>
+        static void Main ()
+        {
+            IIgniteClient DbClient = GetClient();
+            DbClient.GetCluster().SetActive(true);
+            /*
+            foreach (string i in DbClient.GetCacheNames())
+                DbClient.DestroyCache(i);
+            */
+            //CreateDatabase(DbClient);
+
+            Console.WriteLine ("Caches:");
+            foreach (string i in DbClient.GetCacheNames())
+                Console.WriteLine (i);
+            /*
+            Credential c = new Credential
+            {
+                PersonID = 100,
+                Password = "Ag7a0grspaz45!a",
+                Privilegies = 'c',
+                Status = 'n'
+            };
+            */
+            ICacheClient<int, Person> PersonCache = DbClient.GetCache<int, Person> ("EstateAgency");
+            Person p = new Person
+            {
+                Surname = "Ivanov",
+                Name = "Ivan",
+                Phone = "+380962281488",
+                Email = "ivan228@gmail.com",
+                LocationID = 4,
+                Address = "West st., 45",
+                RegDate = DateTime.Today
+            };
+            PersonCache.Put(1, p);
+            foreach (var row in PersonCache.Query(new SqlFieldsQuery("select _key, _val from Persons;")))
+            {
+                Console.WriteLine ("Surname : {0}", (row[1] as Person).Surname);
+                Console.WriteLine ("Name    : {0}", (row[1] as Person).Name);
+                Console.WriteLine ("Phone   : {0}", (row[1] as Person).Phone);
+                Console.WriteLine ("-----------------------------------------------------------");
+            }
+            Console.Read();
+            DbClient.Dispose();
+        }
+
+
+        // -------------- UNUSED ---------------------------------------------
+        [Obsolete("The program is not using Ignite server any more. It should be started from command line using ignite.bat command.", true)]
         public static IIgnite StartIgnitePersistent()
         {
             IgniteConfiguration cfg = new IgniteConfiguration 
@@ -31,40 +324,6 @@ namespace EstateAgencyConsole
             IIgnite ignite = Ignition.Start(cfg);
             ignite.GetCluster().SetActive(true);
             return ignite;
-        }
-
-        static void Main ()
-        {
-            IIgnite ignite = StartIgnitePersistent();
-            ICache<int, Person> Persons = ignite.GetOrCreateCache<int, Person>("persons");
-            Person p = null;
-
-            
-            Person p0 = new Person
-            {
-                Surname = "Іванов",
-                Name = "Іван",
-                Email = "ivanov.ivan@ukr.net",
-                Phone = "+380964449810",
-                LocationID = 1,
-                Address = "вул. Житомирська, буд. 6",
-                RegDate = DateTime.Today
-            };
-            ignite.GetCluster().DisableWal("persons");
-            for (int i=-10001; i<20000; i++)
-            Persons.Put (i, p0);
-            ignite.GetCluster().EnableWal("persons");
-
-            if (Persons.TryGet (0, out p) && p!=null)
-            {
-                Console.WriteLine ($"Surname: {p.Surname} \nName: {p.Name} \nSignup date: {p.RegDate}");
-            } 
-            else
-            {
-                Console.WriteLine ("Key not found.");
-            }
-            Console.Read();
-            ignite.Dispose();
         }
     }
 }
