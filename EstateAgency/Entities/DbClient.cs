088a1296eecf1572f3dc08db96c8358bd2252b87
dmitriynaumov1024 <naumov1024@gmail.com>
2021-04-26 09:35:57 +0300

    DbClient class +

---
+++ EstateAgency/Entities/DbClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache.Query;
+using Apache.Ignite.Core.Client;
+using Apache.Ignite.Core.Client.Cache;
+using Apache.Ignite.Core.Cache.Configuration;
+
+namespace EstateAgency.Database
+{
+    public static partial class DbClient
+    {
+        /// <summary>
+        /// IIgniteClient for interaction with database.
+        /// </summary>
+        static IIgniteClient client = null;
+
+        /// <summary>
+        /// Connect to database.
+        /// </summary>
+        public static void Connect()
+        {
+            client = Ignition.StartClient (new IgniteClientConfiguration 
+                {Endpoints = new[] {"127.0.0.1:10800"}}
+            );
+        }
+
+        /// <summary>
+        /// Disconnect from database.
+        /// </summary>
+        public static void Disconnect()
+        {
+            client.Dispose();
+        }
+    }
+}
---
+++ EstateAgency/Entities/DbCreation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache.Query;
+using Apache.Ignite.Core.Client;
+using Apache.Ignite.Core.Client.Cache;
+using Apache.Ignite.Core.Cache.Configuration;
+
+namespace EstateAgency.Database
+{
+    public static partial class DbClient
+    {
+        public static bool CreateDatabase()
+        {
+            if (client==null) { return false; }
+
+            return true;
+        }
+    }
+}
