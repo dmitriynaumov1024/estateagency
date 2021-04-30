a55d0028145307eb42901c8f2014f24a980caa14
dmitriynaumov1024 <naumov1024@gmail.com>
2021-04-30 20:48:26 +0300

    +Db operation
    Done 4/13 referential integrity checks for put method.

--- EstateAgency/Entities/DbOperation.cs
+++ EstateAgency/Entities/DbOperation.cs
@@ -23,27 +23,181 @@ namespace EstateAgency.Database
         /// <param name="p">Person to put into the cache.</param>
         public static void PutPerson (Person p)
         {
-            if (LocationCache.ContainsKey(p.LocationID))
+            using (var tx = Client.GetTransactions().TxStart())
             {
-                using (var tx = Client.GetTransactions().TxStart())
+                // Check if Location cache contains Location with key p.LocationID, throw error if not.
+                if (!(LocationCache.ContainsKey(p.LocationID)))
                 {
-                    int key = LastUsedKeys.Get("person");
-                    PersonCache.Put(key, p);
-                    key++;
-                    LastUsedKeys.Put ("person", key);
                     tx.Commit();
+                    throw new ReferentialException ("Can not put new entry into Person cache.")
+                    {
+                        Operation = "put",
+                        TableName = "Person",
+                        FieldName = "LocationID",
+                        ReadableMessage = $"Can not put new entry into Person cache because Location with key {p.LocationID} does not exist."
+                    };
                 }
-            } 
-            else
-            {
-                throw new ReferentialException ("Can not put new entry into Persons")
+
+                // Check if Credential cache contains Credential with key p.Phone, throw error if yes.
+                if (CredentialCache.ContainsKey(p.Phone))
                 {
-                    Operation = "put",
-                    TableName = "Persons",
-                    FieldName = "LocationID",
-                    ReadableMessage = $"Can not put new entry into Persons because Location with key {p.LocationID} doest not exist."
-                };
+                    tx.Commit();
+                    throw new ReferentialException ("Can not put new entry into Person cache.")
+                    {
+                        Operation = "put",
+                        TableName = "Person",
+                        FieldName = "Phone",
+                        ReadableMessage = $"Can not put new entry into Person cache because Person with Phone '{p.Phone}' and Credential with the same key already exist."
+                    };
+                }
+
+                // Normal operation.
+                int key = LastUsedKeys.Get("person");
+                PersonCache.Put(key, p);
+                key++;
+                LastUsedKeys.Put ("person", key);
+                tx.Commit();
             }
         }
+
+        /// <summary>
+        /// Put credential in cache and assign string key which is the phone number of the referenced person.
+        /// </summary>
+        /// <param name="value">Credential to put in the cache.</param>
+        public static void PutCredential (Credential value)
+        {
+            using (var tx = Client.GetTransactions().TxStart())
+            {
+                if (PersonCache.ContainsKey(value.PersonID)) 
+                {
+                    CredentialCache.Put (PersonCache.Get(value.PersonID).Phone, value);
+                    tx.Commit();
+                }
+                else 
+                { 
+                    tx.Commit();
+                    throw new ReferentialException ("Can not put new entry into Credential cache.")
+                    {
+                        Operation = "put",
+                        TableName = "Credential",
+                        FieldName = "PersonID",
+                        ReadableMessage = $"Can not put new entry into Credential cache because Person with key {value.PersonID} does not exist."
+                    };
+                }
+            }
+        }
+
+        /// <summary>
+        /// Put agent entity into the cache and assign a key.
+        /// </summary>
+        /// <param name="key">Key to use. Is additionally a back link to person.</param>
+        /// <param name="value">Agent entity to put into the cache.</param>
+        public static void PutAgent (int key, Agent value)
+        {
+            using (var tx = Client.GetTransactions().TxStart())
+            {
+                // Check whether Person cache contains Person with mentioned key. Throw an error if not.
+                if (!(PersonCache.ContainsKey(key)))
+                {
+                    tx.Commit();
+                    throw new ReferentialException ("Can not put new entry into Agent cache.")
+                    {
+                        Operation = "put",
+                        TableName = "Agent",
+                        FieldName = "key",
+                        ReadableMessage = $"Can not put new entry into Agent cache because Person with key {key} does not exist."
+                    };
+                }
+
+                // Normal operation.
+                AgentCache.Put (key, value);
+                tx.Commit();
+            }
+        }
+
+        /// <summary>
+        /// Put location entity into Location cache.
+        /// </summary>
+        /// <param name="value">Location entity to put into cache.</param>
+        public static void PutLocation (Location value)
+        {
+            using (var tx = Client.GetTransactions().TxStart())
+            {
+                int key = LastUsedKeys.Get("location");
+                LocationCache.Put(key, value);
+                key++;
+                LastUsedKeys.Put("location", key);
+                tx.Commit();
+            }
+        }
+
+        /// <summary>
+        /// The one method to put different types of estate objects into caches. <br/>
+        /// Key is chosen automatically. <br/>
+        /// Method checks referential integrity on: <br/>
+        /// EstateObject.SellerID - Person.key. <br/>
+        /// EstateObject.LocationID - Location.key.
+        /// </summary>
+        /// <param name="value">Estate object to put into cache.</param>
+        public static void PutEstateObject (EstateObject value)
+        {
+            using (var tx = Client.GetTransactions().TxStart())
+            {
+                // Error if Person with key = value.SellerID is not found.
+                if (!(PersonCache.ContainsKey(value.SellerID)))
+                {
+                    tx.Commit();
+                    throw new ReferentialException ("Can not put new entry into EstateObject cache.")
+                    {
+                        Operation = "put",
+                        TableName = "EstateObject",
+                        FieldName = "SellerID",
+                        ReadableMessage = $"Can not put new entry into EstateObject cache because Person with key {value.SellerID} does not exist."
+                    };
+                }
+
+                // Error if Location not found.
+                if (!(LocationCache.ContainsKey(value.LocationID)))
+                {
+                    tx.Commit();
+                    throw new ReferentialException ("Can not put new entry into EstateObject cache.")
+                    {
+                        Operation = "put",
+                        TableName = "EstateObject",
+                        FieldName = "LocationID",
+                        ReadableMessage = $"Can not put new entry into EstateObject cache because Location with key {value.LocationID} does not exist."
+                    };
+                }
+
+                // Normal operation.
+                int key = LastUsedKeys.Get("estateobject");
+                switch ((char)value.Variant)
+                {
+                    case 'o':
+                    ObjectCache.Put (key, value);
+                    break;
+
+                    case 'h':
+                    HouseCache.Put (key, (House)value);
+                    break;
+                    
+                    case 'f':
+                    FlatCache.Put (key, (Flat)value);
+                    break;
+
+                    case 'l':
+                    LandplotCache.Put (key, (Landplot)value);
+                    break;
+
+                    default: 
+                    break;
+                }
+                key++;
+                LastUsedKeys.Put("estateobject", key);
+                tx.Commit();
+            }
+        }
+
+
     }
 }
